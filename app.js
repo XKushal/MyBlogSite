a737000819631e23355f79cf618e4b72d06679ca
Xkushal <kushalyhakuri123@gmail.com>
2019-01-05 15:21:51 -0600

    all routes are fixed and cleaned

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
 
 mongoose.connect('mongodb://localhost:27017/BlogApp', { useNewUrlParser: true });
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+
 
 var blogSchema  = new mongoose.Schema({
     title: String,
@@ -45,6 +50,7 @@ app.get("/blogs/new", function(req, res) {
 
 //create route
 app.post("/blogs", function(req, res){
+    req.body.blog.body=req.sanitize(req.body.blog.body); //first body is whatever coming from the form
    //create blog
    Blog.create(req.body.blog, function(err, newBlog){
        if(err){
@@ -66,6 +72,41 @@ app.get("/blogs/:id", function(req, res){
            res.render("show", {blog: foundBlog});
        }
    });
+});
+
+//edit route
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+//update route
+app.put("/blogs/:id", function(req, res){
+    req.body.blog.body=req.sanitize(req.body.blog.body); //first body is whatever coming from the form
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect("/blogs/"+ req.params.id);
+        }
+    });
+});
+
+//destroy route
+app.delete("/blogs/:id", function(req, res){
+    //destroy blog
+    Blog.findByIdAndRemove(req.params.id, function(err){
+         if(err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect("/blogs");
+        }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
